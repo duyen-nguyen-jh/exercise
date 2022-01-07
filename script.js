1f981489fc1d021c4c388a4ae4ce9ac01c971177
jh-kyduyen <duyen.nguyen@journeyh.io>
2022-01-07 23:12:43 +0700

    header animation

--- script.js
+++ script.js
@@ -12,10 +12,14 @@ window.onscroll = function () {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("animatedHeader").classList.add("headerBlack");
     document.getElementById("transLogoHorizontal").classList.remove("transLogoRotate");
+
     document.getElementById("mobileHeader").classList.add("headerBlackMobile");
+    document.getElementById("mobileLogo").classList.remove("mobileLogoRotate");
   } else {
     document.getElementById("animatedHeader").classList.remove("headerBlack");
-    document.getElementById("mobileHeader").classList.remove("headerBlackMobile");
     document.getElementById("transLogoHorizontal").classList.add("transLogoRotate");
+    
+    document.getElementById("mobileHeader").classList.remove("headerBlackMobile");
+    document.getElementById("mobileLogo").classList.add("mobileLogoRotate");
   }
 };
