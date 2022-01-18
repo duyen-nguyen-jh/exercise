b41ad8905081817e6b06467cf3b054f3376ef30c
jh-kyduyen <duyen.nguyen@journeyh.io>
2022-01-18 12:15:34 +0700

    scroll mobile

--- pixture/script.js
+++ pixture/script.js
@@ -69,7 +69,7 @@ function renderScrollItem(data) {
   const { img, title, detail, like, comments } = data;
   const scrollItem = document.createElement("div");
   scrollItem.className = "scrollItem";
-
+  scrollItem.style.display = "inline-block";
   scrollItem.innerHTML = `<div class="imgWrapper">
                 <img src="${img}" alt="img cover 4" />
                 <div class="heartWrapper">
@@ -86,7 +86,7 @@ function renderScrollItem(data) {
                 <div class="itemContentWrapper">
                 <div class="scrollItemTxt">
                     <p class="title">${title}</p>
-                    <p class="detail">${detail}</p>
+                    <p class="detail" style="white-space: normal;">${detail}</p>
                 </div>
                 <div class="itemFooter">
                 <div class="commentWrapper">
@@ -113,20 +113,57 @@ function renderScrollItem(data) {
 
   return scrollItem;
 }
-
-function renderListScroller (list) {
-    const list1Wrapper = document.getElementById("scroller1");
-    list.forEach(element => {
-        const item = renderScrollItem(element);
-        list1Wrapper.appendChild(item);
-    });
+function checkTouchOrClick(e) {
+  return e.changedTouches ? e.changedTouches[0] : e;
 }
-function renderListScroller2 (list) {
-    const list2Wrapper = document.getElementById("scroller2");
-    list.forEach(element => {
-        const item = renderScrollItem(element);
-        list2Wrapper.appendChild(item);
-    });
+
+function renderListScroller(list) {
+  const list1Wrapper = document.getElementById("scroller1");
+  list.forEach((element) => {
+    const item = renderScrollItem(element);
+    list1Wrapper.appendChild(item);
+  });
+
+  // let mouseOnScreenPos = null;
+  // const scrollerContainer = document.getElementById("wrappScroll");
+  list1Wrapper.addEventListener("mousedown", function (e) {
+    list1Wrapper.classList.remove("grab");
+    list1Wrapper.classList.add("grabbing");
+    // mouseOnScreenPos = checkTouchOrClick(e).clientX;
+  });
+  list1Wrapper.addEventListener("touchstart", function (e) {
+    list1Wrapper.classList.remove("grab");
+    list1Wrapper.classList.add("grabbing");
+    // mouseOnScreenPos = checkTouchOrClick(e).clientX;
+  });
+
+  list1Wrapper.addEventListener("mouseup", function (e) {
+    list1Wrapper.classList.remove("grabbing");
+    list1Wrapper.classList.add("grab");
+    // if (mouseOnScreenPos || mouseOnScreenPos === 0) {
+    //   const mouseOutScreenPos = checkTouchOrClick(e).clientX;
+    //   const distance = mouseOutScreenPos - mouseOnScreenPos;
+    //   list1Wrapper.style.transform = `translateX(${distance}px)`
+    //   mouseOnScreenPos = null;
+    // }
+  });
+  list1Wrapper.addEventListener("touchend", function (e) {
+    list1Wrapper.classList.remove("grabbing");
+    list1Wrapper.classList.add("grab");
+    // if (mouseOnScreenPos || mouseOnScreenPos === 0) {
+    //   const mouseOutScreenPos = checkTouchOrClick(e).clientX;
+    //   const distance = mouseOutScreenPos - mouseOnScreenPos;
+    //   list1Wrapper.style.transform = `translateX(${distance}px)`
+    //   mouseOnScreenPos = null;
+    // }
+  });
+}
+function renderListScroller2(list) {
+  const list2Wrapper = document.getElementById("scroller2");
+  list.forEach((element) => {
+    const item = renderScrollItem(element);
+    list2Wrapper.appendChild(item);
+  });
 }
 renderListScroller(sample);
 renderListScroller2(sample);
